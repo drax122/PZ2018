5c332bdd5b4279c08ba4c794f07eb063f8654de4
Martyna <mpyszynska@gmail.com>
2019-01-13 00:35:57 +0100

    profil

--- MicroFB/src/app/user-profile/user-profile.component.ts
+++ MicroFB/src/app/user-profile/user-profile.component.ts
@@ -1,8 +1,10 @@
+import { PostsService } from './../DataServices/posts.service';
 import { Component, OnInit } from '@angular/core';
 import { UserDataServiceService } from '../DataServices/user-data-service.service';
-import {ActivatedRoute} from '@angular/router'
+import {ActivatedRoute} from '@angular/router';
 import { UserDetails } from '../Models/user-details';
 import { FriendsListService } from '../DataServices/friends-list.service';
+import { Post } from '../Models/post';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,15 +12,31 @@ import { FriendsListService } from '../DataServices/friends-list.service';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  UserDetails : UserDetails = new UserDetails({});
-  _IsFriend : boolean; // JESLI TRUE - WYSWIETL SZCZEGOLY I JEGO TABLICE // JESLI FALSE WYSWIETL OKROJONE DANE I GUZIK ZAPROS DO ZNAJOMYCH
-
-  constructor(private userdataService : UserDataServiceService, private route : ActivatedRoute, private friendslistService : FriendsListService) 
-  { 
-
+  UserDetails: UserDetails = new UserDetails({});
+  _IsFriend: boolean; // JESLI TRUE - WYSWIETL SZCZEGOLY I JEGO TABLICE // JESLI FALSE WYSWIETL OKROJONE DANE I GUZIK ZAPROS DO ZNAJOMYCH
+  BoardData: Array<Post> = []; // Tutaj przechowywane są posty z mojej lub aktualnie przeglądanego użytkownika tablicy!
+  postModel = new Post(new Object());
+  constructor(private userdataService : UserDataServiceService,
+    private route: ActivatedRoute, private friendslistService: FriendsListService,
+     private postsService: PostsService)
+  {
+   // LOAD POSTS
+   this.postsService.getFriendBoard(this.getUserId).subscribe(data=>{
+    this.BoardData = data;
+    console.log("BOARD LOADED:");
+    console.dir(this.BoardData);
+  });
   }
   get IsFriend(){
     return this._IsFriend;
+  }
+
+  get getUserId(){
+     return parseInt(this.route.snapshot.paramMap.get('id'));
+  }
+  invite()
+  {
+    this.userdataService.inviteUser(this.getUserId, 1);
   }
   ngOnInit() {
     const Id = parseInt(this.route.snapshot.paramMap.get('id'));
