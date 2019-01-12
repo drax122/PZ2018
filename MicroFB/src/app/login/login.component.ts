f81393e52e2fe35da0b54ec02c5e0d5a071a3ef7
Martyna <mpyszynska@gmail.com>
2019-01-12 17:44:23 +0100

    friendlist dodana

--- MicroFB/src/app/login/login.component.ts
+++ MicroFB/src/app/login/login.component.ts
@@ -1,39 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../auth.service';
-import {User} from '../Models/user';
-import { AppRoutingModule } from '../app-routing.module';
-import { routerNgProbeToken } from '@angular/router/src/router_module';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
+import { User } from "../Models/user";
+import { AppRoutingModule } from "../app-routing.module";
+import { routerNgProbeToken } from "@angular/router/src/router_module";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  userModel = new User('','');
-  ErrorMessage = '';
+  userModel = new User("", "");
+  ErrorMessage = "";
 
-  constructor(private data: DataProviderService, private Auth: AuthService, private router: Router)
+  constructor(private Auth: AuthService, private router: Router) {}
 
-  constructor(private Auth: AuthService, private router: Router) 
+  ngOnInit() {}
 
-  {
-  }
-
-  ngOnInit() {
-  }
-
-  logIn(){
-    this.Auth.loginUser(this.userModel.username,this.userModel.password, (message) =>{
-      // callback
-      if(message.error !== undefined)
-      {
-        this.ErrorMessage = message.error_description;
+  logIn() {
+    this.Auth.loginUser(
+      this.userModel.username,
+      this.userModel.password,
+      message => {
+        // callback
+        if (message.error !== undefined) {
+          this.ErrorMessage = message.error_description;
+        } else {
+          this.router.navigate(["/home"]);
+        }
       }
-      else
-      {
-        this.router.navigate(["/home"]);
-      }
-    });
+    );
   }
 }
